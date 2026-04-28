a05071dff8e8ee0e5e297244e21e54faa3c40905
Jericho <165892976+Antarticstorm@users.noreply.github.com>
2026-04-28 19:57:58 +0800

    Added Menu UI
    -Added Main Menu before the game starts and controls menu
    -Tweaked some numbers in the gravity of the player

---
+++ Assets/Scripts/ControlMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControlMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Control Menu");
+    }
+}
+
